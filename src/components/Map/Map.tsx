7da0fc16efa64e2e3b335a15815a783a9efafbd5
Siestaa <lex.qz97@gmail.com>
2025-03-24 00:28:41 +0500

    fix leafleat marker

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,12 +1,20 @@
 'use client';
 
 import {ILocationMarker} from '@/types/attraction';
+import L from 'leaflet';
 import {useState} from 'react';
 import {MapContainer, Marker, TileLayer} from 'react-leaflet';
 import {MapEventsHandler} from './MapEventsHandler';
 
 export const Map = ({setCoordinates, formData, setLocation}: ILocationMarker) => {
     const [position, setPosition] = useState<[number, number] | null>(null);
+
+    const customIcon = L.icon({
+        iconUrl: '/marker-icon.png',
+        iconSize: [38, 38],
+        iconAnchor: [19, 38],
+        popupAnchor: [0, -38],
+    });
 
     return (
         <MapContainer
@@ -26,6 +34,7 @@ export const Map = ({setCoordinates, formData, setLocation}: ILocationMarker) =>
                         ? position
                         : [formData.coordinates.latitude, formData.coordinates.longitude]
                 }
+                icon={customIcon}
             />
         </MapContainer>
     );
